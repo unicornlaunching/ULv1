644eed3421bf0618c31eea7a53f58287a89dc3dc
unicornlaunching <45397093+unicornlaunching@users.noreply.github.com>
2019-03-02 17:36:05 -0500

    first commit ULv1

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <h1>Yaw</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Now go build something greatsss.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 2 nah boy</Link>
   </Layout>
 )
 
